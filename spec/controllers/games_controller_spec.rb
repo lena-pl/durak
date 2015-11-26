931e5cd4cc536f6256672f7f4eb3b4654f98f2c9
Joseph Bennett <joseph.bennett@powershop.co.nz>
2015-11-27 10:56:34 +1300

    implement game controller new method

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -2,4 +2,23 @@ require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
 
+  describe "GET new" do
+    it "has a 200 status code" do
+      get :new
+
+      expect(response.status).to eq 200
+    end
+
+    it "assigns @game" do
+      get :new
+
+      expect(assigns(:game)).to be_a_new Game
+    end
+
+    it "renders the new game template" do
+      get :new
+
+      expect(response).to render_template :new
+    end
+  end
 end

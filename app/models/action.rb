b719979784a611e72313e30144828cc4c12c91c6
Lena Plaksina <lena.plaksina@gmail.com>
2015-11-28 22:58:32 +1300

    Update model relationships

--- app/models/action.rb
+++ app/models/action.rb
@@ -1,7 +1,8 @@
 class Action < ActiveRecord::Base
-  enum kind:[ :pick_up_from_deck, :pick_up_from_table, :put_down_on_table, :discard ]
+  enum kind:[ :draw_new_card, :pick_up_from_table, :attack_with_card, :defend_against_card, :discard ]
 
   belongs_to :game
   belongs_to :player
-  belongs_to :card
+  belongs_to :active_card, :class_name => 'Card'
+  belongs_to :passive_card, :class_name => 'Card'
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,6 @@
 class Game < ActiveRecord::Base
   belongs_to :trump_card, :class_name => 'Card'
+
+  has_many :actions
+  has_many :players
 end

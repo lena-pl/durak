e2baf74d686bf6966313ec880bca4b2fde102b51
Lena Plaksina <lena.plaksina@gmail.com>
2015-12-01 17:09:52 +1300

    Refactor and complete BuildGameState service

--- app/concepts/game_state.rb
+++ app/concepts/game_state.rb
@@ -1,5 +1,13 @@
 class GameState
+  attr_reader :trump_suit, :cards_in_deck, :player_one_hand, :player_two_hand, :discard_pile, :cards_on_table, :attacker
 
-  def initialize(trump_suit, cards_in_deck, player_1_hand, player_2_hand, cards_on_table, discard_pile, attacker)
+  def initialize(trump_suit, cards_in_deck, player_one_hand, player_two_hand, discard_pile, cards_on_table, attacker)
+    @trump_suit = trump_suit
+    @cards_in_deck = cards_in_deck
+    @player_one_hand = player_one_hand
+    @player_two_hand = player_two_hand
+    @discard_pile = discard_pile
+    @cards_on_table = cards_on_table
+    @attacker = attacker
   end
 end

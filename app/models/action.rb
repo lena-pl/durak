2aed3a0e59b292f471efeff6d1dd93fc84b172b0
Joseph Bennett <joseph.bennett@powershop.co.nz>
2015-12-02 15:00:36 +1300

    add new fields on action to support new actions

--- app/models/action.rb
+++ app/models/action.rb
@@ -1,8 +1,9 @@
 class Action < ActiveRecord::Base
-  enum kind:[ :draw_new_card, :pick_up_from_table, :attack_with_card, :defend_against_card, :discard ]
+  enum kind:[:choose_trump, :deal, :draw_from_deck, :pick_up_from_table, :attack, :defend, :discard]
 
   belongs_to :game
-  belongs_to :player
+  belongs_to :initiating_player, :class_name => 'Player'
+  belongs_to :affected_player, :class_name => 'Player'
   belongs_to :active_card, :class_name => 'Card'
   belongs_to :passive_card, :class_name => 'Card'
 end

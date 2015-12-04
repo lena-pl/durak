8816b4d35e9d0bcedfd30390c3c262b0e0bfcc6f
Lena Plaksina <lena.plaksina@gmail.com>
2015-12-04 15:16:07 +1300

    Refactor apply discard action service and test to work with CardLocation

--- app/services/apply_discard_action.rb
+++ app/services/apply_discard_action.rb
@@ -7,11 +7,12 @@ class ApplyDiscardAction
   def call
     card = @action.active_card
 
-    if !@game_state.card_locations.at(:table).include? card
+    if !@game_state.table.include?(card)
       raise "Card must be on table to discard"
     end
 
-    @game_state.card_locations.move(:table, :discard_pile, card)
+    @game_state.table.move_to(@game_state.discard_pile, card)
+
     @game_state
   end
 end
--- spec/services/apply_discard_action_spec.rb
+++ spec/services/apply_discard_action_spec.rb
@@ -1,27 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe ApplyDiscardAction do
+  fixtures :cards
 
-  let(:card) { Card.create!(:rank => 7, :suit => :hearts) }
-  let(:game) { Game.create!(:trump_card => card) }
+  let(:attacking_card) { cards(:hearts_7) }
+  let(:game) { Game.create!(:trump_card => attacking_card) }
+  let(:initial_game_state) { GameState.base_state(game) }
+  let(:discard_action) { Action.new(:kind => :discard, :active_card => attacking_card) }
 
   describe "#call" do
     context "when there is at least one card on the table" do
-      let(:card_locations) { CardLocations.new(table: [card]) }
-      let(:initial_game_state) { GameState.new(nil, card_locations, nil, nil) }
-      let(:discard_action) { Action.new(:kind => :discard, :active_card => card) }
+      before do
+        initial_game_state.deck.move_to(initial_game_state.table, attacking_card)
+      end
 
       it "moves the card to discard_pile" do
         game_state = ApplyDiscardAction.new(initial_game_state, discard_action).call
-        expect(game_state.card_locations.at(:discard_pile)).to eq [card]
+         expect(game_state.discard_pile.include?(attacking_card)).to be_truthy
+      end
+
+      it "moves the card off the table" do
+        game_state = ApplyDiscardAction.new(initial_game_state, discard_action).call
+        expect(game_state.table.include?(attacking_card)).to be_falsey
       end
     end
 
     context "when there are no cards on the table" do
-      let(:card_locations) { CardLocations.new(table: []) }
-      let(:initial_game_state) { GameState.new(nil, card_locations, nil, nil) }
-      let(:discard_action) { Action.new(:kind => :discard, :active_card => card) }
-
       subject { ApplyDiscardAction.new(initial_game_state, discard_action) }
 
       it "raises error" do

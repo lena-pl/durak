d0f9cd676f7917b5627ef0c5e88099cfd848a63b
Lena Plaksina <lena.plaksina@gmail.com>
2015-12-06 00:17:10 +1300

    Add deal action attacker tests

--- spec/services/apply_deal_action_spec.rb
+++ spec/services/apply_deal_action_spec.rb
@@ -27,5 +27,47 @@ RSpec.describe ApplyDealAction do
         expect(game_state.player_hand(2).include?(card)).to be_truthy
       end
     end
+
+    context "when player one gets the lowest trump card" do
+      let(:deal_action) { Action.new(:kind => :deal, :active_card => cards(:hearts_6), :affected_player => player_one) }
+
+      before do
+        base_game_state.deck.move_to(base_game_state.player_hand(1), cards(:spades_6))
+        base_game_state.deck.move_to(base_game_state.player_hand(2), cards(:hearts_8))
+      end
+
+      it "moves the lowest trump card to player one's hand" do
+        game_state = ApplyDealAction.new(base_game_state, deal_action).call
+        expect(game_state.attacker).to eq player_one
+      end
+    end
+
+    context "when player two gets the lowest trump card" do
+      let(:deal_action) { Action.new(:kind => :deal, :active_card => cards(:hearts_6), :affected_player => player_two) }
+
+      before do
+        base_game_state.deck.move_to(base_game_state.player_hand(2), cards(:spades_6))
+        base_game_state.deck.move_to(base_game_state.player_hand(1), cards(:hearts_8))
+      end
+
+      it "moves the lowest trump card to player one's hand" do
+        game_state = ApplyDealAction.new(base_game_state, deal_action).call
+        expect(game_state.attacker).to eq player_two
+      end
+    end
+
+    context "when neither player has a trump card" do
+      let(:deal_action) { Action.new(:kind => :deal, :active_card => cards(:diamonds_6), :affected_player => player_one) }
+
+      before do
+        base_game_state.deck.move_to(base_game_state.player_hand(1), cards(:spades_6))
+        base_game_state.deck.move_to(base_game_state.player_hand(2), cards(:diamonds_8))
+      end
+
+      it "moves the lowest trump card to player one's hand" do
+        game_state = ApplyDealAction.new(base_game_state, deal_action).call
+        expect(game_state.attacker).to eq player_one
+      end
+    end
   end
 end

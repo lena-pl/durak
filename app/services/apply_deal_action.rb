95bb599bf3455c439e69507a4238c2eb0e1515bd
Lena Plaksina <lena.plaksina@gmail.com>
2015-12-05 23:18:27 +1300

    Add method for determining attacker during dealing

--- app/services/apply_deal_action.rb
+++ app/services/apply_deal_action.rb
@@ -15,6 +15,28 @@ class ApplyDealAction
       @game_state.player_hand(2).add(card)
     end
 
+    @game_state.attacker = find_new_attacker
+
     @game_state
+  end
+
+  private
+
+  def find_new_attacker
+    if lowest_trump(1) < lowest_trump(2)
+      @game_state.player(1)
+    elsif lowest_trump(2) < lowest_trump(1)
+      @game_state.player(2)
+    else
+      @game_state.player(1)
+    end
+  end
+
+  def lowest_trump(player_number)
+    trump_suit_cards(player_number).sort.first
+  end
+
+  def trump_suit_cards(player_number)
+    @game_state.player_hand(player_number).all.select { |card| card.suit == @game_state.trump_card.suit }
   end
 end

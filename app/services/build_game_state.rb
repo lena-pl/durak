634842c00065a2814dd4e8a9ea63cb67066cad05
Joseph Bennett <joseph.bennett@powershop.co.nz>
2015-12-08 16:50:05 +1300

    refactor build game state to remove large if block

--- app/services/build_game_state.rb
+++ app/services/build_game_state.rb
@@ -1,34 +1,20 @@
 class BuildGameState
+  APPLY_ACTION = {
+    deal: ApplyDealAction,
+    draw_from_deck: ApplyDrawFromDeckAction,
+    pickup_from_table: ApplyPickUpFromTableAction,
+    attack: ApplyAttackAction,
+    defend: ApplyDefendAction,
+    discard: ApplyDiscardAction,
+  }
+
   def initialize(game)
-    @actions = game.actions
-    @base_state = GameState.base_state(game)
+    @game = game
   end
 
   def call
-    apply_actions
-  end
-
-  private
-
-  def apply_actions
-    @actions.each do |action|
-      action_service(action).new(@base_state, action).call
+    @game.actions.inject(GameState.base_state(@game)) do |current_game_state, action|
+      APPLY_ACTION[action.kind.to_sym].new(current_game_state, action).call
     end
-  end
-
-  def action_service(action)
-    if action.kind == "deal"
-      ApplyDealAction
-    elsif action.kind == "draw_from_deck"
-      ApplyDrawFromDeckAction
-    elsif action.kind == "pick_up_from_table"
-      ApplyPickUpFromTableAction
-    elsif action.kind == "attack"
-      ApplyAttackAction
-    elsif action.kind == "defend"
-      ApplyDefendAction
-    elsif action.kind == "discard"
-      ApplyDiscardAction
-    end  
   end
 end

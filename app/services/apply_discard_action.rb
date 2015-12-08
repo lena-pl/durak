d05c90dff86f2fd315335a3b87a359f7b02cac55
Joseph Bennett <joseph.bennett@powershop.co.nz>
2015-12-09 10:42:24 +1300

    replace references to initiating_player with player

--- app/services/apply_discard_action.rb
+++ app/services/apply_discard_action.rb
@@ -21,7 +21,7 @@ class ApplyDiscardAction
   private
 
   def find_new_attacker
-    if @action.initiating_player == @game_state.player(1)
+    if @action.player == @game_state.player(1)
       @game_state.player(2)
     else
       @game_state.player(1)

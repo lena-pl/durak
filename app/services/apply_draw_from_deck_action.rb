240bc53344cd1843ea7ec6a4ae25cb63900f018b
Joseph Bennett <joseph.bennett@powershop.co.nz>
2015-12-09 10:42:24 +1300

    replace references to initiating_player with player

--- app/services/apply_draw_from_deck_action.rb
+++ app/services/apply_draw_from_deck_action.rb
@@ -9,9 +9,9 @@ class ApplyDrawFromDeckAction
 
     @game_state.deck.delete(card)
 
-    if @action.initiating_player == @game_state.player(1)
+    if @action.player == @game_state.player(1)
       @game_state.player_hand(1).add(card)
-    elsif @action.initiating_player == @game_state.player(2)
+    elsif @action.player == @game_state.player(2)
       @game_state.player_hand(2).add(card)
     end
 

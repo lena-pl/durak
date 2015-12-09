6fe67adf0ffb42b5046b62bb7b31e8b44d79b887
Lena Plaksina <lena.plaksina@gmail.com>
2015-12-09 16:51:34 +1300

    Validate Card and test validation

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,11 @@
 class Card < ActiveRecord::Base
   enum suit:[ :hearts, :diamonds, :spades, :clubs ]
+
+  validates :suit, presence: true
+  validates :rank, presence: true
+
+  validates :rank, uniqueness: { scope: :suit,
+    message: "should only have 14 cards per suit" }
+
+  validates :rank, numericality: { greater_than: 5, less_than: 15 }
 end

9a5a3755828eba6ca5580481d1ebab16d38a9183
Lena Plaksina <lena.plaksina@gmail.com>
2015-12-10 12:24:32 +1300

    make actions and players dependent on game

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,8 +1,8 @@
 class Game < ActiveRecord::Base
   belongs_to :trump_card, :class_name => 'Card'
 
-  has_many :actions, through: :players
-  has_many :players
+  has_many :actions, through: :players, dependent: :destroy
+  has_many :players, dependent: :destroy
 
   validates :trump_card, presence: true
 end

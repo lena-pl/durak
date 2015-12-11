91e8a1d8b4108f98412208474c12782e2de62fa5
Joseph Bennett <joseph.bennett@powershop.co.nz>
2015-12-11 14:25:07 +1300

    make deal action work with new player_state concept

--- app/services/apply_deal_action.rb
+++ app/services/apply_deal_action.rb
@@ -5,48 +5,36 @@ class ApplyDealAction
   end
 
   def call
+    player_state = @game_state.player_state_for_player(@action.player)
     card = @action.card
 
     @game_state.deck.delete(card)
-
-    if @action.player == @game_state.player(1)
-      @game_state.player_hand(1).add(card)
-    elsif @action.player == @game_state.player(2)
-      @game_state.player_hand(2).add(card)
-    end
+    player_state.hand.push(card)
 
     @game_state.attacker = find_new_attacker
-
     @game_state
   end
 
   private
 
   def find_new_attacker
-    if !has_trump?(1) && !has_trump?(2)
-      @game_state.player(1)
-    elsif !has_trump?(2) && has_trump?(1)
-      @game_state.player(1)
-    elsif !has_trump?(1) && has_trump?(2)
-      @game_state.player(2)
-    elsif lowest_trump(1) < lowest_trump(2)
-      @game_state.player(1)
-    elsif lowest_trump(2) < lowest_trump(1)
-      @game_state.player(2)
+    if lowest_trump
+      lowest_trump[:player]
+    else
+      @game_state.player_states.first.player
     end
   end
 
-  def has_trump?(player_number)
-    !trump_suit_cards(player_number).empty?
+  def lowest_trump
+    lowest_trumps.min_by { |lowest_trump| lowest_trump[:lowest_trump].rank }
   end
 
-  def lowest_trump(player_number)
-    if has_trump?(player_number)
-      trump_suit_cards(player_number).sort.first.rank
+  def lowest_trumps
+    lowest_trumps = @game_state.player_states.map do |player_state|
+      { player: player_state.player,
+        lowest_trump: player_state.lowest_card_with_suit(@game_state.trump_card.suit) }
     end
-  end
 
-  def trump_suit_cards(player_number)
-    @game_state.player_hand(player_number).all.select { |card| card.suit == @game_state.trump_card.suit }
+    lowest_trumps.reject { |lowest_trump| lowest_trump[:lowest_trump].nil? }
   end
 end
--- spec/services/apply_deal_action_spec.rb
+++ spec/services/apply_deal_action_spec.rb
@@ -3,71 +3,200 @@ require 'rails_helper'
 RSpec.describe ApplyDealAction do
   fixtures :cards
 
-  let(:card) { cards(:hearts_7) }
-  let(:game) { Game.create!(:trump_card => card) }
-  let!(:player_one) { Player.create!(:game => game) }
-  let!(:player_two) { Player.create!(:game => game) }
-  let(:base_game_state) { BuildGameState.new(game).call }
+  let(:trump_card) { cards(:hearts_7) }
+  let(:game) { Game.create!(:trump_card => trump_card) }
+  let!(:dealee) { Player.create!(:game => game) }
+  let!(:other_player) { Player.create!(:game => game) }
+  let(:game_state) { BuildGameState.new(game).call }
+  let(:dealee_state) { game_state.player_state_for_player(dealee) }
+  let(:other_player_state) { game_state.player_state_for_player(other_player) }
+
+  let(:next_card) { cards(:clubs_11) }
+  let(:deal_action) { instance_double(Action) }
+  before do
+    allow(deal_action).to receive(:card).and_return next_card
+    allow(deal_action).to receive(:player).and_return dealee
+  end
+  subject { ApplyDealAction.new(game_state, deal_action).call }
 
   describe "#call" do
-    context "player one draws a card from the deck" do
-      let(:deal_action) { Action.new(:kind => :deal, :card => card, :player => player_one) }
-
-      it "moves the card to player one's hand" do
-        game_state = ApplyDealAction.new(base_game_state, deal_action).call
-        expect(game_state.player_hand(1).include?(card)).to be_truthy
+    context "when the dealee is dealt a card" do
+      it "moves the card to their hand" do
+        expect(subject.player_state_for_player(dealee).hand).to include next_card
       end
     end
 
-    context "player two draws a card from the deck" do
-      let(:deal_action) { Action.new(:kind => :deal, :card => card, :player => player_two) }
+    context "when players have no cards" do
+      context "when the dealee is then dealt a non-trump" do
+        let(:next_card) { cards(:spades_7) }
 
-      it "moves the card to player two's hand" do
-        game_state = ApplyDealAction.new(base_game_state, deal_action).call
-        expect(game_state.player_hand(2).include?(card)).to be_truthy
+        it "makes the first player the new attacker" do
+          expect(subject.attacker).to eq game_state.player_states.first.player
+        end
+      end
+
+      context "when the dealee is then dealt a trump" do
+        let(:next_card) { cards(:hearts_9) }
+
+        it "makes the dealee the new attacker" do
+          expect(subject.attacker).to eq dealee
+        end
       end
     end
 
-    context "when player one gets the lowest trump card" do
-      let(:deal_action) { Action.new(:kind => :deal, :card => cards(:hearts_6), :player => player_one) }
-
+    context "when all players have cards" do
       before do
-        base_game_state.deck.move_to(base_game_state.player_hand(1), cards(:spades_6))
-        base_game_state.deck.move_to(base_game_state.player_hand(2), cards(:hearts_8))
+        fill_hand_from_deck(dealee_state, *previously_dealt_dealee)
+        fill_hand_from_deck(other_player_state, *previously_dealt_other_player)
       end
 
-      it "makes player one the new attacker" do
-        game_state = ApplyDealAction.new(base_game_state, deal_action).call
-        expect(game_state.attacker).to eq player_one
+      context "when no players have any trumps" do
+        let(:previously_dealt_dealee) do
+          [cards(:spades_10),
+           cards(:clubs_11)]
+        end
+        let(:previously_dealt_other_player) do
+          [cards(:spades_7),
+           cards(:clubs_8)]
+        end
+
+        context "when the dealee is then dealt a non-trump" do
+          let(:next_card) { cards(:diamonds_7) }
+
+          it "makes the first player the new attacker" do
+            expect(subject.attacker).to eq game_state.player_states.first.player
+          end
+        end
+
+        context "when the dealee is then dealt a trump" do
+          let(:next_card) { cards(:hearts_11) }
+
+          it "makes them the new attacker" do
+            expect(subject.attacker).to eq dealee
+          end
+        end
+      end
+
+      context "when only the other player has a trump" do
+        let(:previously_dealt_dealee) do
+          [cards(:spades_10),
+           cards(:clubs_11)]
+        end
+        let(:previously_dealt_other_player) do
+          [cards(:spades_7),
+           cards(:clubs_8),
+           cards(:hearts_12)]
+        end
+
+        context "when the dealee is then dealt a non-trump" do
+          let(:next_card) { cards(:diamonds_7) }
+
+          it "leaves the other player as the attacker" do
+            expect(subject.attacker).to eq other_player
+          end
+        end
+
+        context "when the dealee is dealt a lower trump than the other player" do
+          let(:next_card) { cards(:hearts_11) }
+
+          it "makes the dealee the new attacker" do
+            expect(subject.attacker).to eq dealee
+          end
+        end
+
+        context "when the dealee is dealt a higher trump than the other player" do
+          let(:next_card) { cards(:hearts_13) }
+
+          it "leaves the other player as the attacker" do
+            expect(subject.attacker).to eq other_player
+          end
+        end
+      end
+
+      context "when both players have trumps" do
+        context "when the dealee has the lowest trump" do
+          let(:previously_dealt_dealee) do
+            [cards(:spades_10),
+             cards(:hearts_7),
+             cards(:hearts_13)]
+          end
+          let(:previously_dealt_other_player) do
+            [cards(:spades_10),
+             cards(:hearts_9),
+             cards(:hearts_8)]
+          end
+
+          context "when the dealee is then dealt a non-trump" do
+            let(:next_card) { cards(:diamonds_7) }
+
+            it "leaves the dealee as the attacker" do
+              expect(subject.attacker).to eq dealee
+            end
+          end
+
+          context "when the dealee is then dealt the lowest trump" do
+            let(:next_card) { cards(:hearts_6) }
+
+            it "leaves the dealee as the attacker" do
+              expect(subject.attacker).to eq dealee
+            end
+          end
+
+          context "when the dealee is then dealt a higher trump" do
+            let(:next_card) { cards(:hearts_14) }
+
+            it "leaves the dealee as the attacker" do
+              expect(subject.attacker).to eq dealee
+            end
+          end
+        end
+
+        context "when the other player has the lowest trump" do
+          let(:previously_dealt_dealee) do
+            [cards(:spades_10),
+             cards(:hearts_9),
+             cards(:hearts_8)]
+          end
+          let(:previously_dealt_other_player) do
+            [cards(:spades_10),
+             cards(:hearts_7),
+             cards(:hearts_13)]
+          end
+
+          context "when the dealee is then dealt a non-trump" do
+            let(:next_card) { cards(:diamonds_7) }
+
+            it "leaves the other_player as the attacker" do
+              expect(subject.attacker).to eq other_player
+            end
+          end
+
+          context "when the dealee is then dealt the lowest trump" do
+            let(:next_card) { cards(:hearts_6) }
+
+            it "makes the dealee the new attacker" do
+              expect(subject.attacker).to eq dealee
+            end
+          end
+
+          context "when the dealee is then dealt a higher trump" do
+            let(:next_card) { cards(:hearts_14) }
+
+            it "leaves the other_player as the attacker" do
+              expect(subject.attacker).to eq other_player
+            end
+          end
+        end
       end
     end
+  end
 
-    context "when player two gets the lowest trump card" do
-      let(:deal_action) { Action.new(:kind => :deal, :card => cards(:hearts_6), :player => player_two) }
+  def fill_hand_from_deck(player_state, *cards)
+    cards.each { |card| move_from_deck_to(player_state, card) }
+  end
 
-      before do
-        base_game_state.deck.move_to(base_game_state.player_hand(2), cards(:spades_6))
-        base_game_state.deck.move_to(base_game_state.player_hand(1), cards(:hearts_8))
-      end
-
-      it "makes player two the new attacker" do
-        game_state = ApplyDealAction.new(base_game_state, deal_action).call
-        expect(game_state.attacker).to eq player_two
-      end
-    end
-
-    context "when neither player has a trump card" do
-      let(:deal_action) { Action.new(:kind => :deal, :card => cards(:diamonds_6), :player => player_one) }
-
-      before do
-        base_game_state.deck.move_to(base_game_state.player_hand(1), cards(:spades_6))
-        base_game_state.deck.move_to(base_game_state.player_hand(2), cards(:diamonds_8))
-      end
-
-      it "makes player one the new attacker" do
-        game_state = ApplyDealAction.new(base_game_state, deal_action).call
-        expect(game_state.attacker).to eq player_one
-      end
-    end
+  def move_from_deck_to(player_state, card)
+    game_state.deck.delete(card)
+    player_state.hand.push(card)
   end
 end

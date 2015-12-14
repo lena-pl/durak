bef7ba4c0aaf451328819dae419041f244c5f9fd
Lena Plaksina <lena.plaksina@gmail.com>
2015-12-15 09:12:18 +1300

    add game over method and related methods

--- app/concepts/game_state.rb
+++ app/concepts/game_state.rb
@@ -14,4 +14,20 @@ class GameState
   def player_state_for_player(player)
     @player_states.find { |player_state| player_state.player == player }
   end
+
+  def game_over?
+    @deck.empty? && durak_found?
+  end
+
+  def durak_found?
+    filled_hands.count == 1
+  end
+
+  def filled_hands
+    @player_states.select { |player_state| !player_state.hand.empty? }
+  end
+
+  def durak
+    filled_hands.first.player
+  end
 end

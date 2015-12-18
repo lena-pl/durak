b7d7abb65a6372d6148fd2dfc0fb7efca8a30cbb
Lena Plaksina <lena.plaksina@gmail.com>
2015-12-18 15:35:46 +1300

    add automatic card dealing after a player discards or picks up from table

--- app/services/draw_cards.rb
+++ app/services/draw_cards.rb
@@ -5,9 +5,9 @@ class DrawCards
 
   def call
     need_to_draw = find_players_who_need_to_draw
-    amount_to_draw = calculate_amount_to_draw(find_players_who_need_to_draw)
+    total_amount_to_draw = total_amount_to_draw(find_players_who_need_to_draw)
 
-    if @game_state.deck.count >= amount_to_draw
+    if @game_state.deck.count >= total_amount_to_draw
       attacker_state = @game_state.player_state_for_player(@game_state.attacker)
 
       if need_to_draw.include?(attacker_state)
@@ -16,7 +16,8 @@ class DrawCards
       end
 
       need_to_draw.each do |player_state|
-        PickUpFromDeck.new(@game_state, player_state, :draw_from_deck).call
+        amount = amount_to_draw(player_state)
+        pick_up_cards_from_deck(player_state, amount)
       end
     end
   end
@@ -29,11 +30,19 @@ class DrawCards
     end
   end
 
-  def calculate_amount_to_draw(players_who_need_to_draw)
+  def total_amount_to_draw(players_who_need_to_draw)
     players_who_need_to_draw.map { |player_state| player_state.hand.count }.reduce(0, &:+)
   end
 
+  def amount_to_draw(player_state)
+    if player_state.hand.count < GameState::STANDARD_HAND_SIZE
+      GameState::STANDARD_HAND_SIZE - player_state.hand.count
+    else
+      0
+    end
+  end
+
   def pick_up_cards_from_deck(player_state, amount)
-    amount.times { PickUpFromDeck.new(@game_state, player_state) }
+    amount.times { PickUpFromDeck.new(@game_state, player_state, :draw_from_deck).call }
   end
 end

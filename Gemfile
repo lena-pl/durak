3cb28e81040815cb8752354aabeddd4be1b9aaa7
Joseph Bennett <joseph.bennett@powershop.co.nz>
2015-12-22 17:06:51 +1300

    add rspec collection matchers

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
   gem 'rspec-rails'
+  gem 'rspec-collection_matchers'
   gem 'simplecov', :require => false
 
   gem 'pry'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,6 +25,8 @@ Coveralls.wear!
 
 RSpec.configure do |config|
 
+  require 'rspec/collection_matchers'
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.

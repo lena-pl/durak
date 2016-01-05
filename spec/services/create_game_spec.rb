37568887652d131b8761318dfb31dbeafbb25ff8
Lena Plaksina <lena.plaksina@gmail.com>
2016-01-06 11:02:58 +1300

    add tests for dealing cards when creating game

--- spec/services/create_game_spec.rb
+++ spec/services/create_game_spec.rb
@@ -2,16 +2,26 @@ require 'rails_helper'
 
 RSpec.describe CreateGame do
   describe "#call" do
-    before do
-      @game = CreateGame.new.call
-    end
+    subject(:game) { CreateGame.new.call }
 
     it "creates a game" do
-      expect(@game).to eq(Game.last)
+      expect(game).to eq Game.last
     end
 
     it "creates 2 players" do
-      expect(@game.players.count).to eq(2)
+      expect(game.players.count).to eq 2
+    end
+
+    let(:game_state) { BuildGameState.new(game).call }
+
+    it "deals the cards to the players" do
+      expect(game_state.deck.count).to eq 24
+
+      hand_counts = game_state.player_states.map do |player_state|
+        player_state.hand.count
+      end
+
+      expect(hand_counts).to all( be 6 )
     end
   end
 end

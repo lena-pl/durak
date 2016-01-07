1f6c7b26ff59cb9784f1088221317aba81260ca0
Lena Plaksina <lena.plaksina@gmail.com>
2016-01-07 15:04:07 +1300

    Remove unused code in build game state spec

--- spec/services/build_game_state_spec.rb
+++ spec/services/build_game_state_spec.rb
@@ -4,36 +4,6 @@ require 'test_game'
 RSpec.describe BuildGameState do
   fixtures :cards
 
-  let(:player_one_initial_hand) do
-    [cards(:diamonds_7),
-     cards(:spades_9),
-     cards(:diamonds_10),
-     cards(:hearts_8),
-     cards(:spades_7),
-     cards(:hearts_12)]
-  end
-
-  let(:player_two_initial_hand) do
-    [cards(:diamonds_8),
-     cards(:hearts_6),
-     cards(:diamonds_11),
-     cards(:spades_11),
-     cards(:spades_14),
-     cards(:hearts_10)]
-  end
-
-  let(:deal_cards) do
-    lambda do |test_game|
-      player_one_initial_hand.each do |card|
-        test_game.deal_card(test_game.game_model.players.first, card)
-      end
-
-      player_two_initial_hand.each do |card|
-        test_game.deal_card(test_game.game_model.players.second, card)
-      end
-    end
-  end
-
   let(:trump_card) { cards(:spades_10) }
   let(:test_game) { TestGame.new(trump_card) }
   subject(:game_state) { BuildGameState.new(test_game.game_model).call }

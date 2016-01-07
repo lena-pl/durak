bd4b598ec361458bc6f712dab6e52687680d59c3
Lena Plaksina <lena.plaksina@gmail.com>
2016-01-07 15:28:39 +1300

    Test games controller show method

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
+  fixtures :cards
+
   describe "GET new" do
     it "has a 200 status code" do
       get :new
@@ -27,6 +29,26 @@ RSpec.describe GamesController, type: :controller do
         post :create
         expect(response).to redirect_to(Game.last)
       end
+    end
+  end
+
+  describe "GET #show" do
+    let(:game) { Game.create!(trump_card: cards(:hearts_6)) }
+    subject { get :show, :id => game }
+
+    it "renders the show template" do
+      expect(subject).to render_template(:show)
+    end
+
+    it "assigns @game" do
+      subject
+      expect(assigns(:game)).to be_a Game
+    end
+
+    it "builds a new game state" do
+      expect_any_instance_of(BuildGameState).to receive(:call)
+      
+      subject
     end
   end
 end

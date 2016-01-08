d36cb568476864b3d5114e68757a63701d953d78
Lena Plaksina <lena.plaksina@gmail.com>
2016-01-08 14:32:49 +1300

    Generate unique token and connected flag for each player as they are created

--- db/migrate/20151126040138_create_players.rb
+++ db/migrate/20151126040138_create_players.rb
@@ -2,6 +2,8 @@ class CreatePlayers < ActiveRecord::Migration
   def change
     create_table :players do |t|
       t.references :game, index: true, null: false
+      t.string :token, null: false, unique: true
+      t.boolean :connected, default: false
       t.timestamps null: false
     end
   end

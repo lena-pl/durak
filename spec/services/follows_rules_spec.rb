84b03637a3d71a2c5c1bf2f4cdf767e6ef2fb13e
Lena Plaksina <lena.plaksina@gmail.com>
2016-01-11 16:09:24 +1300

    Add tests for your move method in follows rules

--- spec/services/follows_rules_spec.rb
+++ spec/services/follows_rules_spec.rb
@@ -19,190 +19,269 @@ RSpec.describe FollowsRules do
       game_state.attacker = attacker
     end
 
-    context "the step kind is defend" do
-      context "the defending suit and rank abide by rules" do
+    context "it is not this player's turn" do
+      context "start of game" do
         let(:defender_hand) do
-          [cards(:spades_8)]
-        end
-
-        let(:attacker_hand) do
           [cards(:spades_7)]
         end
 
-        let(:attack_step) { attacker.steps.create!(kind: :attack, card: cards(:spades_7)) }
-        let(:defend_step) { defender.steps.create!(kind: :defend, card: cards(:spades_8), in_response_to_step: attack_step ) }
-
-        it "passes the rules" do
-          expect(FollowsRules.new(defend_step, game_state, game).call).to eq true
-        end
-      end
-
-      context "the defending suit abides by rules, but the rank does not" do
-        let(:defender_hand) do
+        let(:attacker_hand) do
           [cards(:spades_8)]
         end
 
-        let(:attacker_hand) do
-          [cards(:spades_10)]
-        end
-
-        let(:attack_step) { attacker.steps.create!(kind: :attack, card: cards(:spades_10)) }
-        let(:defend_step) { defender.steps.create!(kind: :defend, card: cards(:spades_8), in_response_to_step: attack_step ) }
+        let(:illegal_step) { defender.steps.create!(kind: :attack, card: cards(:spades_7)) }
 
         it "does not pass rules" do
-          expect(FollowsRules.new(defend_step, game_state, game).call).to eq false
+          expect(FollowsRules.new(illegal_step, game_state, game).call).to eq false
         end
       end
 
-      context "the defending rank abides by rules, but suit does not" do
-        let(:defender_hand) do
-          [cards(:spades_12)]
-        end
-
+      context "mid-turn" do
         let(:attacker_hand) do
-          [cards(:diamonds_7)]
+          [cards(:spades_7),
+           cards(:spades_8)]
         end
 
-        let(:attack_step) { attacker.steps.create!(kind: :attack, card: cards(:diamonds_7)) }
-        let(:defend_step) { defender.steps.create!(kind: :defend, card: cards(:spades_12), in_response_to_step: attack_step ) }
-
-        it "does not pass rules" do
-          expect(FollowsRules.new(defend_step, game_state, game).call).to eq false
-        end
-      end
-
-      context "the defending suit and rank abide by rules due to being a trump" do
         let(:defender_hand) do
-          [cards(:hearts_8)]
+          [cards(:spades_9)]
         end
 
-        let(:attacker_hand) do
-          [cards(:diamonds_14)]
+        context "the attacker tries to move twice in a row" do
+          let!(:attack_step) { attacker.steps.create!(kind: :attack, card: cards(:spades_7)) }
+          let!(:illegal_step) { attacker.steps.create!(kind: :attack, card: cards(:spades_8)) }
+
+          it "does not pass rules" do
+            expect(FollowsRules.new(illegal_step, game_state, game).call).to eq false
+          end
         end
 
-        let(:attack_step) { attacker.steps.create!(kind: :attack, card: cards(:diamonds_14)) }
-        let(:defend_step) { defender.steps.create!(kind: :defend, card: cards(:hearts_8), in_response_to_step: attack_step ) }
+        context "the defender tries to attack" do
+          let!(:illegal_step) { defender.steps.create!(kind: :attack, card: cards(:spades_9)) }
 
-        it "passes the rules" do
-          expect(FollowsRules.new(defend_step, game_state, game).call).to eq true
-        end
-      end
-
-      context "the attacking card is a trump and the defending suit and rank abide by rules" do
-        let(:defender_hand) do
-          [cards(:hearts_8)]
+          it "does not pass rules" do
+            expect(FollowsRules.new(illegal_step, game_state, game).call).to eq false
+          end
         end
 
-        let(:attacker_hand) do
-          [cards(:hearts_9)]
-        end
+        context "the defender tries to defend prematurely" do
+          let!(:draw_step) { defender.steps.create!(kind: :draw_from_deck, card: cards(:spades_6)) }
+          let!(:draw_step) { attacker.steps.create!(kind: :draw_from_deck, card: cards(:spades_10)) }
+          let!(:illegal_step) { defender.steps.create!(kind: :defend, card: cards(:spades_9), in_response_to_step: draw_step) }
 
-        let(:attack_step) { attacker.steps.create!(kind: :attack, card: cards(:hearts_8)) }
-        let(:defend_step) { defender.steps.create!(kind: :defend, card: cards(:hearts_9), in_response_to_step: attack_step ) }
-
-        it "passes the rules" do
-          expect(FollowsRules.new(defend_step, game_state, game).call).to eq true
-        end
-      end
-
-      context "the attacking card is a trump and the defending suit abides by rules, but rank does not" do
-        let(:defender_hand) do
-          [cards(:hearts_8)]
-        end
-
-        let(:attacker_hand) do
-          [cards(:hearts_7)]
-        end
-
-        let(:attack_step) { attacker.steps.create!(kind: :attack, card: cards(:hearts_8)) }
-        let(:defend_step) { defender.steps.create!(kind: :defend, card: cards(:hearts_7), in_response_to_step: attack_step ) }
-
-        it "does not pass the rules" do
-          expect(FollowsRules.new(defend_step, game_state, game).call).to eq false
+          it "does not pass rules" do
+            expect(FollowsRules.new(illegal_step, game_state, game).call).to eq false
+          end
         end
       end
     end
 
-    context "the step kind is attack" do
-      context "the first attack of the turn" do
-        let(:attacker_hand) do
-          [cards(:spades_7)]
-        end
-
+    context "it is this player's turn" do
+      context "the old defender draws from deck, becomes attacker and attacks" do
         let(:defender_hand) do
           [cards(:spades_8)]
         end
 
-        let(:attack_step) { attacker.steps.create!(kind: :attack, card: cards(:spades_7)) }
+        let(:attacker_hand) do
+          [cards(:spades_7)]
+        end
+
+        let!(:draw_step) { defender.steps.create!(kind: :draw_from_deck, card: cards(:spades_6)) }
+        let!(:legal_step) { defender.steps.create!(kind: :attack, card: cards(:spades_6)) }
+
+        before do
+          game_state.attacker = defender
+        end
 
         it "passes the rules" do
-          expect(FollowsRules.new(attack_step, game_state, game).call).to eq true
+          expect(FollowsRules.new(legal_step, game_state, game).call).to eq true
         end
       end
 
-      context "subsequent attacks" do
-        context "the attacking rank abides by the rules" do
-          let(:attacker_hand) do
-            [cards(:spades_7),
-             cards(:diamonds_7)]
-          end
-
+      context "the step kind is defend" do
+        context "the defending suit and rank abide by rules" do
           let(:defender_hand) do
-            [cards(:spades_8),
-             cards(:spades_10)]
+            [cards(:spades_8)]
           end
 
-          let!(:attack_step) { attacker.steps.create!(kind: :attack, card: cards(:spades_7)) }
-          let!(:defend_step) { defender.steps.create!(kind: :defend, card: cards(:spades_8), in_response_to_step: attack_step ) }
-          let!(:next_attack_step) { attacker.steps.create!(kind: :attack, card: cards(:diamonds_7)) }
+          let(:attacker_hand) do
+            [cards(:spades_7)]
+          end
+
+          let(:attack_step) { attacker.steps.create!(kind: :attack, card: cards(:spades_7)) }
+          let(:defend_step) { defender.steps.create!(kind: :defend, card: cards(:spades_8), in_response_to_step: attack_step ) }
 
           it "passes the rules" do
-            ApplyAttackStep.new(game_state, attack_step).call
-            ApplyDefendStep.new(game_state, defend_step).call
-            expect(FollowsRules.new(next_attack_step, game_state, game).call).to eq true
+            expect(FollowsRules.new(defend_step, game_state, game).call).to eq true
           end
         end
 
-        context "the attacking rank does not abide by the rules" do
-          let(:attacker_hand) do
-            [cards(:spades_7),
-             cards(:diamonds_12)]
-          end
-
+        context "the defending suit abides by rules, but the rank does not" do
           let(:defender_hand) do
-            [cards(:spades_8),
-             cards(:spades_10)]
+            [cards(:spades_8)]
           end
 
-          let!(:attack_step) { attacker.steps.create!(kind: :attack, card: cards(:spades_7)) }
-          let!(:defend_step) { defender.steps.create!(kind: :defend, card: cards(:spades_8), in_response_to_step: attack_step ) }
-          let!(:next_attack_step) { attacker.steps.create!(kind: :attack, card: cards(:diamonds_12)) }
+          let(:attacker_hand) do
+            [cards(:spades_10)]
+          end
+
+          let(:attack_step) { attacker.steps.create!(kind: :attack, card: cards(:spades_10)) }
+          let(:defend_step) { defender.steps.create!(kind: :defend, card: cards(:spades_8), in_response_to_step: attack_step ) }
+
+          it "does not pass rules" do
+            expect(FollowsRules.new(defend_step, game_state, game).call).to eq false
+          end
+        end
+
+        context "the defending rank abides by rules, but suit does not" do
+          let(:defender_hand) do
+            [cards(:spades_12)]
+          end
+
+          let(:attacker_hand) do
+            [cards(:diamonds_7)]
+          end
+
+          let(:attack_step) { attacker.steps.create!(kind: :attack, card: cards(:diamonds_7)) }
+          let(:defend_step) { defender.steps.create!(kind: :defend, card: cards(:spades_12), in_response_to_step: attack_step ) }
+
+          it "does not pass rules" do
+            expect(FollowsRules.new(defend_step, game_state, game).call).to eq false
+          end
+        end
+
+        context "the defending suit and rank abide by rules due to being a trump" do
+          let(:defender_hand) do
+            [cards(:hearts_8)]
+          end
+
+          let(:attacker_hand) do
+            [cards(:diamonds_14)]
+          end
+
+          let(:attack_step) { attacker.steps.create!(kind: :attack, card: cards(:diamonds_14)) }
+          let(:defend_step) { defender.steps.create!(kind: :defend, card: cards(:hearts_8), in_response_to_step: attack_step ) }
+
+          it "passes the rules" do
+            expect(FollowsRules.new(defend_step, game_state, game).call).to eq true
+          end
+        end
+
+        context "the attacking card is a trump and the defending suit and rank abide by rules" do
+          let(:defender_hand) do
+            [cards(:hearts_8)]
+          end
+
+          let(:attacker_hand) do
+            [cards(:hearts_9)]
+          end
+
+          let(:attack_step) { attacker.steps.create!(kind: :attack, card: cards(:hearts_8)) }
+          let(:defend_step) { defender.steps.create!(kind: :defend, card: cards(:hearts_9), in_response_to_step: attack_step ) }
+
+          it "passes the rules" do
+            expect(FollowsRules.new(defend_step, game_state, game).call).to eq true
+          end
+        end
+
+        context "the attacking card is a trump and the defending suit abides by rules, but rank does not" do
+          let(:defender_hand) do
+            [cards(:hearts_8)]
+          end
+
+          let(:attacker_hand) do
+            [cards(:hearts_7)]
+          end
+
+          let(:attack_step) { attacker.steps.create!(kind: :attack, card: cards(:hearts_8)) }
+          let(:defend_step) { defender.steps.create!(kind: :defend, card: cards(:hearts_7), in_response_to_step: attack_step ) }
 
           it "does not pass the rules" do
-            ApplyAttackStep.new(game_state, attack_step).call
-            ApplyDefendStep.new(game_state, defend_step).call
-            expect(FollowsRules.new(next_attack_step, game_state, game).call).to eq false
+            expect(FollowsRules.new(defend_step, game_state, game).call).to eq false
           end
         end
+      end
 
-        context "there are not enough cards in defender's hand to defend" do
+      context "the step kind is attack" do
+        context "the first attack of the turn" do
           let(:attacker_hand) do
-            [cards(:spades_7),
-             cards(:diamonds_7)]
+            [cards(:spades_7)]
           end
 
           let(:defender_hand) do
             [cards(:spades_8)]
           end
 
-          let!(:attack_step) { attacker.steps.create!(kind: :attack, card: cards(:spades_7)) }
-          let!(:defend_step) { defender.steps.create!(kind: :defend, card: cards(:spades_8), in_response_to_step: attack_step ) }
-          let!(:next_attack_step) { attacker.steps.create!(kind: :attack, card: cards(:diamonds_7)) }
+          let(:attack_step) { attacker.steps.create!(kind: :attack, card: cards(:spades_7)) }
 
-          it "does not pass the rules" do
-            ApplyAttackStep.new(game_state, attack_step).call
-            ApplyDefendStep.new(game_state, defend_step).call
-            expect(FollowsRules.new(next_attack_step, game_state, game).call).to eq false
+          it "passes the rules" do
+            expect(FollowsRules.new(attack_step, game_state, game).call).to eq true
+          end
+        end
+
+        context "subsequent attacks" do
+          context "the attacking rank abides by the rules" do
+            let(:attacker_hand) do
+              [cards(:spades_7),
+               cards(:diamonds_7)]
+            end
+
+            let(:defender_hand) do
+              [cards(:spades_8),
+               cards(:spades_10)]
+            end
+
+            let!(:attack_step) { attacker.steps.create!(kind: :attack, card: cards(:spades_7)) }
+            let!(:defend_step) { defender.steps.create!(kind: :defend, card: cards(:spades_8), in_response_to_step: attack_step ) }
+            let!(:next_attack_step) { attacker.steps.create!(kind: :attack, card: cards(:diamonds_7)) }
+
+            it "passes the rules" do
+              ApplyAttackStep.new(game_state, attack_step).call
+              ApplyDefendStep.new(game_state, defend_step).call
+              expect(FollowsRules.new(next_attack_step, game_state, game).call).to eq true
+            end
+          end
+
+          context "the attacking rank does not abide by the rules" do
+            let(:attacker_hand) do
+              [cards(:spades_7),
+               cards(:diamonds_12)]
+            end
+
+            let(:defender_hand) do
+              [cards(:spades_8),
+               cards(:spades_10)]
+            end
+
+            let!(:attack_step) { attacker.steps.create!(kind: :attack, card: cards(:spades_7)) }
+            let!(:defend_step) { defender.steps.create!(kind: :defend, card: cards(:spades_8), in_response_to_step: attack_step ) }
+            let!(:next_attack_step) { attacker.steps.create!(kind: :attack, card: cards(:diamonds_12)) }
+
+            it "does not pass the rules" do
+              ApplyAttackStep.new(game_state, attack_step).call
+              ApplyDefendStep.new(game_state, defend_step).call
+              expect(FollowsRules.new(next_attack_step, game_state, game).call).to eq false
+            end
+          end
+
+          context "there are not enough cards in defender's hand to defend" do
+            let(:attacker_hand) do
+              [cards(:spades_7),
+               cards(:diamonds_7)]
+            end
+
+            let(:defender_hand) do
+              [cards(:spades_8)]
+            end
+
+            let!(:attack_step) { attacker.steps.create!(kind: :attack, card: cards(:spades_7)) }
+            let!(:defend_step) { defender.steps.create!(kind: :defend, card: cards(:spades_8), in_response_to_step: attack_step ) }
+            let!(:next_attack_step) { attacker.steps.create!(kind: :attack, card: cards(:diamonds_7)) }
+
+            it "does not pass the rules" do
+              ApplyAttackStep.new(game_state, attack_step).call
+              ApplyDefendStep.new(game_state, defend_step).call
+              expect(FollowsRules.new(next_attack_step, game_state, game).call).to eq false
+            end
           end
         end
       end

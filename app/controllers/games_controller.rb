c0114a4ec3e9072d2f70122dfb7aa4d5625912d4
Lena Plaksina <lena.plaksina@gmail.com>
2016-01-14 14:25:32 +1300

    ID players by their tokens, don't show tokens in URL

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,15 +5,16 @@ class GamesController < ApplicationController
 
   def create
     @game = CreateGame.new.call
-    @current_player = session[:current_player] = @game.players.first
+    @current_player = @game.players.first
+    session[:current_player] = @current_player.token
 
-    redirect_to controller: 'games', action: 'show', id: @game.id, token: @game.players.first.token
+    redirect_to controller: 'games', action: 'show', id: @game.id
   end
 
   def show
     @game = Game.find(params[:id])
     @game_state = BuildGameState.new(@game).call
-    @current_player = current_player
+    @current_player = @game.players.find_by!(token: session[:current_player])
 
     if @game.players.first.connected && @game.players.second.connected
       render :show, layout: !request.xhr?
@@ -25,17 +26,9 @@ class GamesController < ApplicationController
   def join
     @game = Game.find(params[:id])
     @game.players.second.update_attributes!(connected: true)
-    @current_player = session[:current_player] = @game.players.second
+    @current_player = @game.players.second
+    session[:current_player] = @current_player.token
 
-    redirect_to controller: 'games', action: 'show', id: @game.id, token: @game.players.second.token
-  end
-
-  private
-
-  def current_player
-    if params[:token]
-      session[:current_player] = @game.players.where('token = ?', params[:token]).first
-    end
-    session[:current_player]
+    redirect_to controller: 'games', action: 'show', id: @game.id
   end
 end

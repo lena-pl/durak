a01f4d10f5d47af847e9b41411a7bce069b2713b
Lena Plaksina <lena.plaksina@gmail.com>
2016-01-15 16:53:37 +1300

    Add more iterations to the trump card randomness test

--- spec/services/select_random_trump_card_spec.rb
+++ spec/services/select_random_trump_card_spec.rb
@@ -11,5 +11,13 @@ RSpec.describe SelectRandomTrumpCard do
     it "selects a card from one of available suits" do
       expect(["hearts", "diamonds", "spades", "clubs"]).to include(card.suit)
     end
+
+    it "generates 100 random cards of correct rank and suit" do
+      100.times do
+        card = SelectRandomTrumpCard.new.call
+        expect(card.rank).to be_between 6,14
+        expect(["hearts", "diamonds", "spades", "clubs"]).to include(card.suit)
+      end
+    end
   end
 end

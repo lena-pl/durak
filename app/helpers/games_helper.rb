aa2e8103b3960dd8518d06888fbd694875bae1f3
Lena Plaksina <lena.plaksina@gmail.com>
2016-01-19 11:28:23 +1300

    Create game helper methods and remove redundant partials

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,47 @@
 module GamesHelper
+  def player_name(player)
+    if player == @game.players.first
+      "Player 1"
+    elsif player == @game.players.second
+      "Player 2"
+    end
+  end
+
+  def opponent
+    if @current_player == @game_state.attacker
+      @game_state.defender
+    else
+      @game_state.attacker
+    end
+  end
+
+  def suit_colour(suit)
+    if suit == "hearts" || suit == "diamonds"
+      "red"
+    else
+      "black"
+    end
+  end
+
+  def display_rank(rank)
+    return rank unless rank > 10
+
+    if rank == 11
+      "J"
+    elsif rank == 12
+      "Q"
+    elsif rank == 13
+      "K"
+    elsif rank == 14
+      "A"
+    end
+  end
+
+  def display_suit_ascii(suit)
+    if suit == "diamonds"
+      "&diams;"
+    else
+      "&#{suit};"
+    end
+  end
 end

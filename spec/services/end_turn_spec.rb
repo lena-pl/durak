9a1eeadb3d7a5ad400816c2beffe14b1e6210f45
Lena Plaksina <lena.plaksina@gmail.com>
2016-01-28 10:03:11 +1300

    Add end turn spec

--- spec/services/end_turn_spec.rb
+++ spec/services/end_turn_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe PlayCard do
+RSpec.describe EndTurn do
   fixtures :cards
 
   let(:game) { Game.create!(trump_card: cards(:hearts_6)) }
@@ -17,15 +17,20 @@ RSpec.describe PlayCard do
         player_two.steps.create!(kind: :deal, card: cards(:spades_9))
       end
 
-      context "player one attacks" do
+      context "player one discards" do
+        before do
+          player_one.steps.create!(kind: :attack, card: cards(:spades_7))
+          player_two.steps.create!(kind: :defend, card: cards(:spades_8), in_response_to_step: game.steps.last)
+        end
+
         it "calls try to apply step service exactly once" do
           expect_any_instance_of(TryToApplyStep).to receive(:call).once
 
-          PlayCard.new(player_one, cards(:spades_7).id, game).call
+          EndTurn.new(player_one, game).call
         end
 
         it "does not return any errors" do
-          service = PlayCard.new(player_one, cards(:spades_7).id, game)
+          service = EndTurn.new(player_one, game)
 
           service.call
 
@@ -35,16 +40,16 @@ RSpec.describe PlayCard do
         it "passes the correct arguments to try to apply step" do
           apply = instance_double(TryToApplyStep)
 
-          expect(TryToApplyStep).to receive(:new).with(game: game, player: player_one, step_kind: :attack, card_id: cards(:spades_7).id, in_response_to_step: nil).and_return(apply)
+          expect(TryToApplyStep).to receive(:new).with(game: game, player: player_one, step_kind: :discard).and_return(apply)
 
           expect(apply).to receive(:call)
           allow(apply).to receive(:errors).and_return([])
 
-          PlayCard.new(player_one, cards(:spades_7).id, game).call
+          EndTurn.new(player_one, game).call
         end
       end
 
-      context "player two defends" do
+      context "player two picks up" do
         before do
           player_one.steps.create!(kind: :attack, card: cards(:spades_7))
         end
@@ -52,11 +57,11 @@ RSpec.describe PlayCard do
         it "calls try to apply step service exactly once" do
           expect_any_instance_of(TryToApplyStep).to receive(:call).once
 
-          PlayCard.new(player_two, cards(:spades_8).id, game).call
+          EndTurn.new(player_two, game).call
         end
 
         it "does not return any errors" do
-          service = PlayCard.new(player_two, cards(:spades_8).id, game)
+          service = EndTurn.new(player_two, game)
 
           service.call
 
@@ -66,12 +71,12 @@ RSpec.describe PlayCard do
         it "passes the correct arguments to try to apply step" do
           apply = instance_double(TryToApplyStep)
 
-          expect(TryToApplyStep).to receive(:new).with(game: game, player: player_two, step_kind: :defend, card_id: cards(:spades_8).id, in_response_to_step: game.steps.last).and_return(apply)
+          expect(TryToApplyStep).to receive(:new).with(game: game, player: player_two, step_kind: :pick_up_from_table).and_return(apply)
 
           expect(apply).to receive(:call)
           allow(apply).to receive(:errors).and_return([])
 
-          PlayCard.new(player_two, cards(:spades_8).id, game).call
+          EndTurn.new(player_two, game).call
         end
       end
     end

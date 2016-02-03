1500ab3d7a20a43154e945947bee55ea6d9fc380
Lena Plaksina <lena.plaksina@gmail.com>
2016-02-03 15:35:44 +1300

    Extract copy to clipboard js to own file

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -58,33 +58,3 @@ $(document).ready(function() {
     $('.player-actions .new_step').submit();
   });
 });
-
-// Copy to clipboard
-(function() {
-  // click events
-  document.body.addEventListener('click', copy, true);
-  // event handler
-  function copy(e) {
-    // find target element
-    var
-      t = e.target,
-      c = t.dataset.copytarget,
-      inp = (c ? document.querySelector(c) : null);
-
-    // is element selectable?
-    if (inp && inp.select) {
-
-      // select text
-      inp.select();
-
-      try {
-        // copy text
-        document.execCommand('copy');
-        inp.blur();
-      }
-      catch (err) {
-        alert('please press Ctrl/Cmd+C to copy');
-      }
-    }
-  }
-})();
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.assets.precompile += %w(copy_to_clipboard.js)
 end
